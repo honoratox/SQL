ede22c06eb302b2202b5cd439fc573aa5b7892c5
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2024-07-29 12:24:54 -0300

    Update cfbCursos_criacao.sql

--- MySQL/cfbCursos_criacao.sql
+++ MySQL/cfbCursos_criacao.sql
@@ -1,6 +1,7 @@
 create table cliente (
-	i_cliente_cliente int primary key not null auto_increment,
-    s_nomecliente_cliente varchar(50) not null,
+	-- utilizando um padrão pra criação, x_yyyy_zzzz = tipo de variável(int(i), string(s), data(d).../nome do campo / nome da tabela de origem
+    i_cliente_cliente int primary key not null auto_increment, 
+    s_nomecliente_cliente varchar(50) not null, -- usando var char pois é uma variável dinâmica
     s_cpf_cliente varchar(11) not null,
     d_datanascimento_cliente datetime
 );
